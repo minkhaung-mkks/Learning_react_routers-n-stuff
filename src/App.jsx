1d550ce1d25c14fd6990800597aedd2424d354e1
Min Khaung <minkhaung.mkks@gmail.com>
2023-09-24 01:27:10 +0630

    Installed Prop-types

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { useState } from 'react'
+import { Route, useNavigate, Routes } from 'react-router-dom'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  const navigate = useNavigate()
   return (
     <>
     </>
