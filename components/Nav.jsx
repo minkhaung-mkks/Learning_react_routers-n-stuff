3068e6bd8f26db750aabde9f8512667dd3763108
Min Khaung <minkhaung.mkks@gmail.com>
2023-09-24 20:19:25 +0630

    Nav Component

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,9 +1,29 @@
+import { Link } from 'react-router-dom'
 
-const Nav = () => {
+import PropTypes from 'prop-types'
+Nav.propTypes = {
+    search: PropTypes.string,
+    setSearch: PropTypes.func
+}
+const Nav = ({ search, setSearch }) => {
     return (
-        <div>
-
-        </div>
+        <nav className='nav'>
+            <form className='searchForm' onSubmit={(e) => e.preventDefault()}>
+                <label htmlFor="search">Search Posts</label>
+                <input
+                    id="search"
+                    type="text"
+                    placeholder='Search Posts'
+                    value={search}
+                    onChange={(e) => setSearch(e.target.value)}
+                />
+            </form>
+            <ul>
+                <li><Link to={'/'}>Home</Link></li>
+                <li><Link to={'/post'}>Posts</Link></li>
+                <li><Link to={'/about'}>About</Link></li>
+            </ul>
+        </nav>
     )
 }
 
