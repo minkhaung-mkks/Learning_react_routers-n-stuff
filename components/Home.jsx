7b305be5b7d1adb338cad963991d94871419d4b5
Min Khaung <minkhaung.mkks@gmail.com>
2023-09-24 20:19:50 +0630

    Basic Home Component

--- components/Home.jsx
+++ components/Home.jsx
@@ -1,10 +1,21 @@
 
-
-const Home = () => {
+import PropTypes from 'prop-types'
+import Feed from './Feed'
+Home.propTypes = {
+    posts: PropTypes.array
+}
+const Home = ({ posts }) => {
     return (
-        <div>
-
-        </div>
+        <main className="home">
+            {posts.length ? (
+                <Feed posts={posts} />
+            ) : (
+                <p style={{ marginTop: "2rem" }}>
+                    No posts found.
+                </p>
+            )
+            }
+        </main >
     )
 }
 
