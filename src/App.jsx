417a8250894d93a15cba9465a62a32d2ae66526f
Min Khaung <minkhaung.mkks@gmail.com>
2023-09-24 20:20:20 +0630

    Fully ready to implement routing and fetching

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,30 @@ function App() {
   const navigate = useNavigate()
   const [searchResults, setSearchResults] = useState()
   const [search, setSearch] = useState()
-  const [posts, setPosts] = useState()
+  const [posts, setPosts] = useState([{
+    id: 1,
+    title: "My First Post",
+    datetime: "July 01, 2021 11:17:36 AM",
+    body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
+  },
+  {
+    id: 2,
+    title: "My 2nd Post",
+    datetime: "July 01, 2021 11:17:36 AM",
+    body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
+  },
+  {
+    id: 3,
+    title: "My 3rd Post",
+    datetime: "July 01, 2021 11:17:36 AM",
+    body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
+  },
+  {
+    id: 4,
+    title: "My Fourth Post",
+    datetime: "July 01, 2021 11:17:36 AM",
+    body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
+  }])
   const [postTitle, setPostTitle] = useState()
   const [postBody, setPostBody] = useState()
   const handleSubmit = () => { }
