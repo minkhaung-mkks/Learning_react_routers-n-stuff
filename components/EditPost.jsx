cd6f448cb89fdc61c27b41519066df3994276a51
Min Khaung <minkhaung.mkks@gmail.com>
2023-10-01 23:51:35 +0630

    Edit Component Finished

--- components/EditPost.jsx
+++ components/EditPost.jsx
@@ -15,14 +15,27 @@ const EditPost = ({ posts, handleEdit, editTitle, setEditTitle, editBody, setEdi
     }, [post, setEditBody, setEditTitle])
     return (
         <main className="NewPost">
-            <h2>Edit Post</h2>
-            <form onSubmit={(e) => e.preventDefault()} className="newPostForm">
-                <label htmlFor="postTitle">Title:</label>
-                <input type="text" id='postTitle' required value={editTitle} onChange={(e) => setEditTitle(e.target.value)} />
-                <label htmlFor="postBody">Post:</label>
-                <input type="text" id='postBody' required value={editBody} onChange={(e) => setEditBody(e.target.value)} />
-                <button type="submit" onClick={() => handleEdit(post.id)}>Update</button>
-            </form>
+            {editTitle ? (
+                <>
+                    <h2>Edit Post</h2>
+                    <form onSubmit={(e) => e.preventDefault()} className="newPostForm">
+                        <label htmlFor="postTitle">Title:</label>
+                        <input type="text" id='postTitle' required value={editTitle} onChange={(e) => setEditTitle(e.target.value)} />
+                        <label htmlFor="postBody">Post:</label>
+                        <input type="text" id='postBody' required value={editBody} onChange={(e) => setEditBody(e.target.value)} />
+                        <button type="submit" onClick={() => handleEdit(post.id)}>Update</button>
+                    </form>
+                </>
+            ) :
+                (
+                    <>
+                        <h2>Post not Found</h2>
+                        <p>
+                            <Link to={'/'}>Go back Home</Link>
+                        </p>
+                    </>
+                )
+            }
         </main>
     )
 }
